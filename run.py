d7c811eef92b7d49a0ee3daffec30c48c208f044
touchme <vodkavoid@proton.me>
2026-05-18 12:51:27 +0300

    the rest cleanup

--- run.py
+++ run.py
@@ -4,13 +4,13 @@ from bot import dp, main
 import logging
 
 from handlers.user.start import start_router
-from handlers.user.payment_test import payment_router
+from handlers.user.payment_test import payment_test_router
 
 logging.basicConfig(level=logging.INFO)
 
 main_router = Router()
 main_router.include_router(start_router)
-main_router.include_router(payment_router)
+main_router.include_router(payment_test_router)
 dp.include_router(main_router)
 
 if __name__ == '__main__':
