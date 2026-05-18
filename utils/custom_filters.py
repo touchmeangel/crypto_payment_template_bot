c1920b9523d5b206fcb2102c58a853eb930220b5
touchme <vodkavoid@proton.me>
2026-05-18 12:22:14 +0300

    +cleanup

--- utils/custom_filters.py
+++ utils/custom_filters.py
@@ -7,11 +7,3 @@ from config import ADMIN_ID_LIST
 class AdminIdFilter(BaseFilter):
     async def __call__(self, message: types.Message):
         return message.from_user.id in ADMIN_ID_LIST
-    
-class IsUserExistFilter(BaseFilter):
-  async def __call__(self, message: types.Message):
-      return await UserService.is_exist(message.from_user.id)
-
-class UserSubscriptionValidFilter(BaseFilter):
-  async def __call__(self, message: types.Message):
-      return await UserService.is_subscription_valid(message.from_user.id)
--- utils/login_code.py
+++
@@ -1,5 +0,0 @@
-import re
-
-def validate_login_code(login_code: str):
-  digits_only = re.sub(r"[^\d]+", "", login_code)
-  return digits_only
