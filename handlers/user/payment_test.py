abfc96db13502e07391341054b25df5a5d26ad3e
touchme <vodkavoid@proton.me>
2026-05-18 12:50:43 +0300

    added payment test

--- handlers/user/payment_test.py
+++ handlers/user/payment_test.py
@@ -2,13 +2,10 @@ from typing import Union
 from aiogram import types, Router
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.filters.callback_data import CallbackData
-from utils.custom_filters import IsUserExistFilter
 from aiogram.types import CallbackQuery, Message
 from handlers.common.common import send_message, send_to_admins
 from aiogram.fsm.context import FSMContext
-from config import PRICE_30_DAYS, BASE_RPC
-from services.user import UserService
-from language import LanguageService
+from config import BASE_RPC
 from crypto_api.TRC20 import TRC20
 from crypto_api.BEP20 import BEP20
 from crypto_api.ERC20 import ERC20
@@ -24,50 +21,54 @@ assets = [
   TRC20("USDT", "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t"),
 ]
 
-payment_router = Router()
+payment_test_router = Router()
 
-class SubscriptionCallback(CallbackData, prefix="subscription"):
+class PaymentTestCallback(CallbackData, prefix="payment_test"):
   level: int
   price: float
   asset_i: int
 
-def create_callback_subscription(level: int, price: float = -1, asset_i: int = -1):
-  return SubscriptionCallback(level=level, price=price, asset_i=asset_i).pack()
+def create_payment_test_callback(level: int, price: float = -1, asset_i: int = -1):
+  return PaymentTestCallback(level=level, price=price, asset_i=asset_i).pack()
 
-async def subscription(message: Union[Message, CallbackQuery]):
-  from handlers.user.my_profile import create_callback_profile
+async def payment_test(message: Union[Message, CallbackQuery]):
+  from handlers.user.start import create_callback_start
 
-  telegram_id = message.chat.id if isinstance(message, Message) else message.from_user.id
-  language_code = await UserService.get_language_code(telegram_id)
-  
-  msg = LanguageService.get_translation(language_code, "subscription_selection")
-  days = LanguageService.get_translation(language_code, "days")
-  subscription_30_days_button = types.InlineKeyboardButton(text=f"30 {days} | {PRICE_30_DAYS}$", callback_data=create_callback_subscription(1, price=PRICE_30_DAYS))
-  back_button = types.InlineKeyboardButton(text=LanguageService.get_translation(language_code, "back"), callback_data=create_callback_profile(0))
-  markup = types.InlineKeyboardMarkup(inline_keyboard=[[subscription_30_days_button], [back_button]])
-  await send_message(message, msg, reply_markup=markup)
+  payment_test_0_usd_button = types.InlineKeyboardButton(text=f"0$ (test this before making any transfers)", callback_data=create_payment_test_callback(1, price=0))
+  payment_test_5_usd_button = types.InlineKeyboardButton(text=f"5$", callback_data=create_payment_test_callback(1, price=5))
+  payment_test_10_usd_button = types.InlineKeyboardButton(text=f"10$", callback_data=create_payment_test_callback(1, price=10))
+  payment_test_25_usd_button = types.InlineKeyboardButton(text=f"25$", callback_data=create_payment_test_callback(1, price=25))
+  payment_test_50_usd_button = types.InlineKeyboardButton(text=f"50$", callback_data=create_payment_test_callback(1, price=50))
+  payment_test_100_usd_button = types.InlineKeyboardButton(text=f"100$", callback_data=create_payment_test_callback(1, price=100))
+  back_button = types.InlineKeyboardButton(text="Back", callback_data=create_callback_start(0))
+  markup = types.InlineKeyboardMarkup(inline_keyboard=[[
+    payment_test_0_usd_button,
+    payment_test_5_usd_button,
+    payment_test_10_usd_button,
+    payment_test_25_usd_button,
+    payment_test_50_usd_button,
+    payment_test_100_usd_button
+  ], [back_button]])
+  await send_message(message, "Select an amount", reply_markup=markup)
 
 async def crypto_payment_selection(callback: CallbackQuery):
-  telegram_id = callback.from_user.id
-  language_code = await UserService.get_language_code(telegram_id)
-  unpacked_callback = SubscriptionCallback.unpack(callback.data)
+  unpacked_callback = PaymentTestCallback.unpack(callback.data)
 
-  crypto_selection = LanguageService.get_translation(language_code, "crypto_selection")
-  msg = (f"{crypto_selection} (<b>{unpacked_callback.price}$</b>)")
+  msg = (f"Select a crypto asset to use (<b>{unpacked_callback.price}$</b>)")
 
-  markup = await get_crypto_payment_markup(unpacked_callback.price, language_code)
+  markup = await get_crypto_payment_markup(unpacked_callback.price)
   await send_message(callback, msg, reply_markup=markup)
 
-async def get_crypto_payment_markup(price: float, language_code: str):
+async def get_crypto_payment_markup(price: float):
   keyboard_builder = InlineKeyboardBuilder()
   option_buttons = []
 
   for asset_i, asset in enumerate(assets):
     display = f"{await asset.network} | {await asset.symbol}"
-    option_buttons.append(types.InlineKeyboardButton(text=display, callback_data=create_callback_subscription(2, price=price, asset_i=asset_i)))
+    option_buttons.append(types.InlineKeyboardButton(text=display, callback_data=create_payment_test_callback(2, price=price, asset_i=asset_i)))
 
   keyboard_builder.add(*option_buttons)
-  back_button = types.InlineKeyboardButton(text=LanguageService.get_translation(language_code, "back"), callback_data=create_callback_subscription(0))
+  back_button = types.InlineKeyboardButton(text="Back", callback_data=create_payment_test_callback(0))
   keyboard_builder.add(back_button)
   adj = len(option_buttons) * [1] + [1]
   keyboard_builder.adjust(*adj)
@@ -75,32 +76,27 @@ async def get_crypto_payment_markup(price: float, language_code: str):
   return keyboard_builder.as_markup()
 
 async def crypto_payment(callback: CallbackQuery, state: FSMContext):
-  telegram_id = callback.from_user.id
-  language_code = await UserService.get_language_code(telegram_id)
-  unpacked_callback = SubscriptionCallback.unpack(callback.data)
+  unpacked_callback = PaymentTestCallback.unpack(callback.data)
 
   asset = assets[unpacked_callback.asset_i]
 
-  crypto_deposit_warning = LanguageService.get_translation(language_code, "crypto_deposit_warning")
-  crypto_deposit_amount = LanguageService.get_translation(language_code, "crypto_deposit_amount")
-  crypto_address = LanguageService.get_translation(language_code, "crypto_address")
   symbol = await asset.symbol
   price_usd = await asset.price_usd()
   deposit_amount = unpacked_callback.price / price_usd
 
   secret_key, public_key = await asset.create_wallet()
-  msg = (f'<a href="{await asset.link}">{await asset.network} | {symbol}</a> {crypto_deposit_warning}\n\n'
-         f'{crypto_deposit_amount}<code>{round(deposit_amount, 6)}</code> <b>{symbol}</b>\n'
-         f'{crypto_address}: <code>{public_key}</code>')
+  msg = (f'<a href="{await asset.link}">{await asset.network} | {symbol}</a> (<b>Any other assets will be lost</b>)\n\n'
+         f'Required deposit of <code>{round(deposit_amount, 6)}</code> <b>{symbol}</b>\n'
+         f'Address: <code>{public_key}</code>')
   
-  check_button = types.InlineKeyboardButton(text=LanguageService.get_translation(language_code, "check"), callback_data=create_callback_subscription(3, price=unpacked_callback.price, asset_i=unpacked_callback.asset_i))
-  cancel_button = types.InlineKeyboardButton(text=LanguageService.get_translation(language_code, "cancel"), callback_data=create_callback_subscription(0))
+  check_button = types.InlineKeyboardButton(text="Check", callback_data=create_payment_test_callback(3, price=unpacked_callback.price, asset_i=unpacked_callback.asset_i))
+  cancel_button = types.InlineKeyboardButton(text="Cancel", callback_data=create_payment_test_callback(0))
   markup = types.InlineKeyboardMarkup(inline_keyboard=[[check_button], [cancel_button]])
   await state.update_data(secret_key=secret_key, public_key=public_key, deposit_amount=deposit_amount)
   await send_message(callback, msg, reply_markup=markup)
   
 async def check_payment(callback: CallbackQuery, state: FSMContext):
-  from handlers.user.my_profile import my_profile
+  from handlers.user.start import start
 
   data = await state.get_data()
   await state.clear()
@@ -109,7 +105,7 @@ async def check_payment(callback: CallbackQuery, state: FSMContext):
   deposit_amount = data.get("deposit_amount")
 
   telegram_id = callback.from_user.id
-  unpacked_callback = SubscriptionCallback.unpack(callback.data)
+  unpacked_callback = PaymentTestCallback.unpack(callback.data)
 
   asset = assets[unpacked_callback.asset_i]
 
@@ -124,15 +120,15 @@ async def check_payment(callback: CallbackQuery, state: FSMContext):
                f'ID: <code>{telegram_id}</code>\n'
                f'Username: <b>{callback.from_user.username or "unknown"}</b>\n'
                f'Wallet: <span class=\"tg-spoiler\">{secret_key}</span>')
-  await asyncio.gather(send_to_admins(admin_msg), UserService.subscribe_user_for_3_months(telegram_id))
-  return await my_profile(callback)
+  await send_to_admins(admin_msg)
+  return await start(callback)
 
-@payment_router.callback_query(SubscriptionCallback.filter(), IsUserExistFilter())
-async def navigate(callback: CallbackQuery, state: FSMContext, callback_data: SubscriptionCallback):
+@payment_test_router.callback_query(PaymentTestCallback.filter())
+async def navigate(callback: CallbackQuery, state: FSMContext, callback_data: PaymentTestCallback):
   current_level = callback_data.level
 
   levels = {
-    0: subscription,
+    0: payment_test,
     1: crypto_payment_selection,
     2: crypto_payment,
     3: check_payment
--- handlers/user/start.py
+++ handlers/user/start.py
@@ -3,18 +3,11 @@ from aiogram.filters import Command
 from aiogram.filters.callback_data import CallbackData
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from handlers.common.common import get_back_to_menu_button, send_message
-from handlers.user.my_profile import create_callback_profile
-from handlers.user.accounts import create_callback_accounts
-from handlers.user.autobuy import create_callback_autobuy
 from utils.custom_filters import AdminIdFilter
 import aiogram.types as types
 import aiogram.enums as enums
 from typing import Union
 from bot import bot
-
-from config import CHANNEL_LINK, CHANNEL_NAME, SUPPORT_LINK, FAQ_LINK, REQUIRED_CHANNEL_IDS, REQUIRED_CHANNEL_LINKS, REQUIRED_CHANNEL_NAMES
-from language import LanguageService
-from services.user import UserService
 
 start_router = Router()
 
@@ -26,52 +19,12 @@ def create_callback_start(level: int) -> str:
 
 @start_router.message(Command(commands=["start"]))
 async def start(message: Union[types.Message, types.CallbackQuery]):
-  user_telegram_id = message.chat.id if isinstance(message, types.Message) else message.from_user.id
-  user_telegram_username = message.from_user.username
-  await UserService.user_logged(user_telegram_id, user_telegram_username)
-  await check_subs(message)
+  await send_message(message, "Please support this <a href=\"https://github.com/touchmeangel/crypto_payment_template_bot\">repo</a> with a star 🥺", reply_markup=main_markup())
 
-async def check_subs(message: Union[types.Message, types.CallbackQuery]):
-  user_telegram_id = message.chat.id if isinstance(message, types.Message) else message.from_user.id
-  language_code = await UserService.get_language_code(user_telegram_id)
+def main_markup():
+  test_button = types.InlineKeyboardButton(text="PAYMENT TEST", callback_data=create_payment_test_callback(0))
 
-  chats_to_join = []
-  for chat_i, chat_id in enumerate(REQUIRED_CHANNEL_IDS):     
-    member = await bot.get_chat_member(chat_id, user_telegram_id)
-    if not member.status in [enums.ChatMemberStatus.ADMINISTRATOR, enums.ChatMemberStatus.CREATOR, enums.ChatMemberStatus.MEMBER]:
-      chats_to_join.append(chat_i)
-  
-  if len(chats_to_join) > 0:
-    join = LanguageService.get_translation(language_code, "join")
-    msg = (f"{join}\n\n")
-    for chat_i in chats_to_join:
-      msg += f"<a href=\"{REQUIRED_CHANNEL_LINKS[chat_i]}\"><b>{REQUIRED_CHANNEL_NAMES[chat_i]}</b></a>\n"
-    check_button = types.InlineKeyboardButton(text=LanguageService.get_translation(language_code, "check"), callback_data=create_callback_start(2))
-    markup = types.InlineKeyboardMarkup(inline_keyboard=[[check_button]])
-    await send_message(message, msg, reply_markup=markup)
-    if isinstance(message, types.CallbackQuery):
-      await message.answer()
-  else:
-    await send_message(message, LanguageService.get_translation(language_code, "hello"), reply_markup=main_markup(language_code))
-
-def main_markup(language_code: str):
-  auto_buy = types.InlineKeyboardButton(text=LanguageService.get_translation(language_code, "autobuy"), callback_data=create_callback_autobuy(0))
-  accounts = types.InlineKeyboardButton(text=LanguageService.get_translation(language_code, "accounts"), callback_data=create_callback_accounts(0))
-  my_profile_button = types.InlineKeyboardButton(text=LanguageService.get_translation(language_code, "profile"), callback_data=create_callback_profile(0))
-  faq_button = types.InlineKeyboardButton(text=LanguageService.get_translation(language_code, "faq"), url=FAQ_LINK)
-  help_button = types.InlineKeyboardButton(text=LanguageService.get_translation(language_code, "help"), callback_data=create_callback_start(1))
-
-  channel_button = types.InlineKeyboardButton(text=CHANNEL_NAME, url=CHANNEL_LINK)
-  return types.InlineKeyboardMarkup(inline_keyboard=[[auto_buy], [accounts, my_profile_button], [faq_button, help_button], [channel_button]])
-
-async def support(callback: types.CallbackQuery):
-  language_code = await UserService.get_language_code(callback.from_user.id)
-  keyboard_builder = InlineKeyboardBuilder()
-
-  keyboard_builder.button(text=LanguageService.get_translation(language_code, "support"), url=SUPPORT_LINK)
-  keyboard_builder.add(get_back_to_menu_button(language_code))
-  keyboard_builder.adjust(1)
-  await send_message(callback, LanguageService.get_translation(language_code, "qa"), reply_markup=keyboard_builder.as_markup())
+  return types.InlineKeyboardMarkup(inline_keyboard=[[test_button]])
 
 @start_router.callback_query(StartCallback.filter())
 async def start_menu_navigation(callback: types.CallbackQuery, callback_data: StartCallback):
@@ -79,8 +32,6 @@ async def start_menu_navigation(callback: types.CallbackQuery, callback_data: St
 
   levels = {
     0: start,
-    1: support,
-    2: check_subs
   }
 
   current_level_function = levels[current_level]
